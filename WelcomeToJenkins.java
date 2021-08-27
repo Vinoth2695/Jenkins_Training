a725ee84ff61210aef47f16e4a6f3a262f38e7cb
Vinoth2695 <89629425+Vinoth2695@users.noreply.github.com>
2021-08-27 15:44:59 +0530

    Update WelcomeToJenkins.java

--- WelcomeToJenkins.java
+++ WelcomeToJenkins.java
@@ -3,6 +3,6 @@ public class WelcomeToJenkins
 	public static void main(String args[])  
 {  
 	//prints the string  
-	System.out.println("Welcome to Jenkins Integration");  
+	System.out.println("Welcome to Jenkins Integration Vinoth");  
 }  
 }  
